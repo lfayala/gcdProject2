fe8ca60db76c403610984b3fa052f5ccc86dfe37
lfayala <layala@ufm.edu>
2016-08-21 15:31:54 -0600

    Delete SummarizedData.R

--- SummarizedData.R
+++
@@ -1,7 +0,0 @@
-#Creating the summarized tidy dataframe
-summarizedData<-function(originalData){
-  library(dplyr)
-  originalData$AcSubject<-factor(originalData$AcSubject)
-  groupedData<-group_by(originalData,ActivityLabel,AcSubject)
-  summarize_each(groupedData,funs(mean),2:79)
-}
